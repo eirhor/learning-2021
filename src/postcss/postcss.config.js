11d69ec347d6e6cadaa4984fb9f645be8eab3b1b
Eirik Horvath <horvath.eirik@gmail.com>
2021-08-31 12:47:04 +0200

    postcss media query

--- src/postcss/postcss.config.js
+++ src/postcss/postcss.config.js
@@ -1,5 +1,17 @@
 module.exports = {
     plugins: [
-        require('autoprefixer')
+        require('autoprefixer'),
+        require('postcss-custom-media')({ importFrom: [
+                {
+                    customMedia: {
+                        '--mobile-landscape': '(orientation: landscape)',
+                        '--medium': '(min-width: 640px)',
+                        '--tablet-landscape': '(min-width: 1024px and orientation: landscape)',
+                        '--laptop': '(min-width: 1070px)',
+                        '--desktop': '(min-width: 1920px)',
+                        '--monitor': '(min-width: 2560px)'
+                    }
+                },
+            ]})
     ]
 }
