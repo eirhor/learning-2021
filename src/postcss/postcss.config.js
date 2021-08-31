5fdefdd365749484c9ec8e34ad616e876fbb8875
Eirik Horvath <horvath.eirik@gmail.com>
2021-08-31 13:24:29 +0200

    Update mq defs

--- src/postcss/postcss.config.js
+++ src/postcss/postcss.config.js
@@ -4,12 +4,11 @@ module.exports = {
         require('postcss-custom-media')({ importFrom: [
                 {
                     customMedia: {
-                        '--mobile-landscape': '(orientation: landscape)',
+                        '--landscape': '(orientation: landscape)',
                         '--medium': '(min-width: 640px)',
-                        '--tablet-landscape': '(min-width: 1024px and orientation: landscape)',
-                        '--laptop': '(min-width: 1070px)',
-                        '--desktop': '(min-width: 1920px)',
-                        '--monitor': '(min-width: 2560px)'
+                        '--large': '(min-width: 1024px)',
+                        '--large-landscape': '(min-width: 1024px and orientation: landscape)',
+                        '--xlarge': '(min-width: 2560px)'
                     }
                 },
             ]})
