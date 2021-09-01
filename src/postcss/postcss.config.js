ec2f01ed3c4f37baa0da4a2d728b906c6936616a
Eirik Horvath <horvath.eirik@gmail.com>
2021-09-01 08:49:31 +0200

    Refactor postcss-media-queries to get intellisense

--- src/postcss/postcss.config.js
+++ src/postcss/postcss.config.js
@@ -2,15 +2,7 @@ module.exports = {
     plugins: [
         require('autoprefixer'),
         require('postcss-custom-media')({ importFrom: [
-                {
-                    customMedia: {
-                        '--landscape': '(orientation: landscape)',
-                        '--medium': '(min-width: 640px)',
-                        '--large': '(min-width: 1024px)',
-                        '--large-landscape': '(min-width: 1024px and orientation: landscape)',
-                        '--xlarge': '(min-width: 2560px)'
-                    }
-                },
+                './global/media-queries.css'
             ]})
     ]
 }
